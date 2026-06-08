0805624e30646b304f2331e649abf02cd37e68c7
Ankit Bisht <ankit1289pl@gmail.com>
2026-06-09 00:47:02 +0530

    Create Diamond_Star_Pattern.py

---
+++ pattern/Diamond_Star_Pattern.py
@@ -0,0 +1,28 @@
+# Given an integer N, print the following pattern : 
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+
+for i in range(0, 5):
+    # spaces
+    for j in range(5, i, -1):
+        print(' ', end='')
+    # stars
+    for k in range(0, i+1):
+        print('*', end=' ')
+    print()
+for i in range(0, 5):
+    # spaces
+    for j in range(0, i+1):
+        print(' ', end='')
+    # stars
+    for k in range(5,i,-1):
+        print('*', end=' ')
+    print()
