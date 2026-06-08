d11d9fa05f5148b05d0fe685680a337c7c27a1c2
Ankit Bisht <ankit1289pl@gmail.com>
2026-06-09 00:38:35 +0530

    Create Inverted_Star_Pyramid.py

---
+++ pattern/Inverted_Star_Pyramid.py
@@ -0,0 +1,15 @@
+# Given an integer N, print the following pattern : 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+
+for i in range(0, 5):
+    # spaces
+    for j in range(0, i+1):
+        print(' ', end='')
+    # stars
+    for k in range(5,i,-1):
+        print('*', end=' ')
+    print()
