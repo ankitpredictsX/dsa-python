1e2d15a72b531a34fda4cadd3843e7b79140b321
Ankit Bisht <ankit1289pl@gmail.com>
2026-06-09 20:18:33 +0530

    Create Hollow_Rectangle.py

---
+++ pattern/Hollow_Rectangle.py
@@ -0,0 +1,22 @@
+# Given an integer N, print the following pattern : 
+
+*****
+*   *
+*   *
+*   *
+*****
+
+
+# Outer loop for rows
+n=5
+for i in range(n):
+            # Inner loop for columns
+    for j in range(n):
+                # Print star if it's a border cell
+        if i == 0 or j == 0 or i == n - 1 or j == n - 1:
+                    print("*", end="")
+                # Print space otherwise
+        else:
+                    print(" ", end="")
+            # Move to next line after each row
+    print()
