5d3964932b8a57bd68059982d49a5f98952821c9
Ankit Bisht <ankit1289pl@gmail.com>
2026-06-09 10:08:33 +0530

    Create Increasing_Number_Triangle.py

---
+++ pattern/Increasing_Number_Triangle.py
@@ -0,0 +1,13 @@
+# Given an integer N, print the following pattern :
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+num=1
+for i in range(5):
+    for j in range(0,i+1):
+        print(num,end=' ')
+        num+=1
+    print()
