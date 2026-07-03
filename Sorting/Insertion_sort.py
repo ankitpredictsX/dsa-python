8923dae482d98618796eabc4ffaf88f6cbacbf4e
Ankit Bisht <ankit1289pl@gmail.com>
2026-07-03 21:48:41 +0530

    Update Insertion_sort.py

--- Sorting/Insertion_sort.py
+++ Sorting/Insertion_sort.py
@@ -2,23 +2,18 @@
 
 # It works like sorting playing cards in your hand: you pick one card and place it in its correct position among the already sorted cards
 
-def insertion_sort(arr):
+def insertion_sort_swap(arr):
     n = len(arr)
 
     for i in range(1, n):
-        key = arr[i]
-        j = i - 1
+        j = i
 
-        # Move elements greater than key one position ahead
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
             j -= 1
-
-        # Place key at its correct position
-        arr[j + 1] = key
 
     return arr
 
 
-numbers = [5, 3, 8, 4, 2]
-print(insertion_sort(numbers))
+numbers = [64, 25, 12, 22, 11]
+print(insertion_sort_swap(numbers))
